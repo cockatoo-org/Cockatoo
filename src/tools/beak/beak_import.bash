9184f2a0c6eae66b1c028d95fbff2b3a95de49b1
crumbjp <hiroaki.kubota@mail.crumb.jp>
2012-02-25 10:52:07 +0900

    tool: beak_imoport.bash

---
+++ src/tools/beak/beak_import.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+TOOL_DIR=`dirname $0`
+export PATH=/usr/local/php/bin:$PATH
+if [ "$COCKATOO_CONF" = "" ]; then
+    export COCKATOO_CONF=${TOOL_DIR}/../../config.php
+fi
+php ${TOOL_DIR}/beak_import.php $*
