91a9cd5378482f3ad5d4080713d515f4c43a64c7
crumbjp <hiroaki.kubota@mail.crumb.jp>
2012-05-30 19:20:26 +0900

    config : support function-type

--- src/operation/html/lib/common.js
+++ src/operation/html/lib/common.js
@@ -140,61 +140,6 @@ exports.crawl_object = function ( data , cbobj ) {
   crawl_object_impl(data,cbobj,[],[],[],undefined);
 }
 
-/*
-exports.crawl_object = function ( data , enter_callback , igns , path , done , leave_callback ) {
-  function type_check( type ){
-    for ( var i in igns ){
-      if ( igns[i] === type ) {
-        return false;
-      }
-    }
-    return true;
-  }
-  if ( igns === undefined )  igns = [];
-  if ( done === undefined )  done = [];
-  if ( path === undefined )  path = [];
-  if ( done.length === 0 ){
-    enter_callback('',data,path,0,false,null); // root object
-    done.push(data);
-  }
-  if ( leave_callback === undefined ) {
-    leave_callback = function ( data , path ) {};
-  }
-  for ( var i in data ){
-    var circulation_flg = false;
-    path.push(i);
-    var type = typeof(data[i]);
-    if ( type === 'object') {
-      // Check circulation
-      for ( var no in done ) {
-	if ( done[no] === data[i] ) { 
-	  circulation_flg = true;
-	  break;
-	}
-      }
-      if ( ! circulation_flg ) {
-	done.push(data[i]);
-      }
-      // Callback
-      var objid = done.length;
-      if ( type_check(type)){
-	if ( enter_callback(i,data[i],path,objid,circulation_flg,data) ) {
-	  this.crawl_object ( data[i],enter_callback,igns,path ,done , leave_callback );
-	}
-      }else{
-	this.crawl_object ( data[i],enter_callback,igns,path ,done , leave_callback);
-      }
-    }else{
-      if ( type_check(type)){
-	enter_callback(i,data[i],path,undefined,circulation_flg,data);
-      }
-    }
-    path.pop(i);
-  }
-  leave_callback(data,path);
-}
-*/
-
 // Extend string
 String.prototype.replaceAll = function (org, dest){  
   return this.split(org).join(dest);  
