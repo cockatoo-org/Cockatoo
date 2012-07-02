569a91ef2598f0e986586f26c94573f654b91ea9
Hiroaki Kubota <cat.s.panch@gmail.com>
2012-07-02 16:46:46 +0900

    yslowviewer sample

---
+++ src/wwwutils/plugin/yslowviewer/YslowviewerRequestParser.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * YslowviewerRequestParser.php - ????
+ *  
+ * @package ????
+ * @access public
+ * @author hiroaki.kubota <hiroaki.kubota@mail.rakuten.com> 
+ * @create 2012/02/01
+ * @version $Id$
+ * @copyright Copyright (C) 2012, rakuten 
+ */
+namespace yslowviewer;
+class YslowviewerRequestParser extends \Cockatoo\DefaultRequestParser {
+  public function parseImpl(){
+    if ( preg_match('@^/yslowviewer/(.*)?$@', $this->reqpath , $matches ) !== 0 ) {
+      // application = yslowviewer
+      $this->service = 'yslowviewer';
+      $this->device  = 'default';
+      $this->session_path = '/yslowviewer';
+      $this->path    = $matches[1];
+    }elseif ( preg_match('@^/([^/]+)/([^/]+)/(.*)?$@', $this->reqpath , $matches ) !== 0 ) {
+      $this->service = $matches[1];
+      $this->device  = $matches[2];
+      $this->path    = $matches[3];
+      $this->session_path = '/'.$matches[1];
+      return; // other application
+    }
+    throw new \Exception('Unexpect PATH:' . $this->reqpath);
+  }
+}
