fb1b3a0be13221b9f20c1d33a50ce3dd93a43338
Hiroaki Kubota <cat.s.panch@gmail.com>
2012-07-12 22:03:15 -0700

    plugin

---
+++ src/wwwutils/plugin/Cockatoo/SampleDeviceSelector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * SampleDeviceSelector.php - ????
+ *  
+ * @package ????
+ * @access public
+ * @author  <hiroaki.kubota@mail.rakuten.com> 
+ * @create 2012/03/07
+ * @version $Id$
+ * @copyright Copyright (C) 2012, rakuten 
+ */
+namespace Cockatoo;
+class SampleDeviceSelector extends DefaultDeviceSelector {
+  protected     $deviceTree = array('android'=>'default');
+}
