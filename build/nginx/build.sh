329b5c646b2849ada8c8d828dc1f171bd04882af
crumbjp <hiroaki.kubota@mail.crumb.jp>
2013-06-05 12:17:24 +0900

    nginx settings

--- build/nginx/build.sh
+++ build/nginx/build.sh
@@ -32,21 +32,30 @@ function build_nginx(){
     run pushd ${NAME}
     run ./configure                        \
 	--prefix=/usr/local/nginx-${VERSION} \
+	--without-http_ssi_module                 \
+	--without-http_autoindex_module           \
+	--without-http_geo_module                 \
+	--without-http_map_module                 \
+	--without-http_memcached_module           \
+	--without-http_empty_gif_module           \
+	--without-http_browser_module             \
 	--with-http_ssl_module             \
 	--with-http_spdy_module            \
-	--with-http_realip_module          \
-	--with-http_addition_module        \
-	--with-http_xslt_module            \
-	--with-http_sub_module             \
-	--with-http_dav_module             \
-	--with-http_gunzip_module          \
 	--with-http_gzip_static_module     \
-	--with-http_random_index_module    \
-	--with-http_secure_link_module     \
-	--with-http_degradation_module     \
 	--with-http_stub_status_module     \
 	--with-pcre                        \
 	--with-pcre-jit                    
+
+#	--with-http_realip_module          \
+#	--with-http_xslt_module            \
+#	--with-http_sub_module             \
+#	--with-http_dav_module             \
+#	--with-http_gunzip_module          \
+#	--with-http_random_index_module    \
+#	--with-http_secure_link_module     \
+#	--with-http_degradation_module     \
+#	--with-http_addition_module        \
+
     run make
     run make install DESTDIR=${ROOT}
     run ln -sfT nginx-${VERSION} ${ROOT}/usr/local/nginx
@@ -69,3 +78,23 @@ fi
 # Edit /etc/sysconfig/iptables.conf
 # -A RH-Firewall-1-INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
 # sudo /etc/init.d/iptables condrestart
+
+
+
+#  --without-http_split_clients_module       \
+#  --without-http_charset_module             \
+#  --without-http_gzip_module                \
+#  --without-http_userid_module              \
+#  --without-http_access_module              \
+#  --without-http_auth_basic_module          \
+#  --without-http_referer_module             \
+#  --without-http_rewrite_module             \
+#  --without-http_proxy_module               \
+#  --without-http_fastcgi_module             \
+#  --without-http_uwsgi_module               \
+#  --without-http_scgi_module                \
+#  --without-http_limit_conn_module          \
+#  --without-http_limit_req_module           \
+#  --without-http_upstream_ip_hash_module    \
+#  --without-http_upstream_least_conn_module \
+#  --without-http_upstream_keepalive_module  \
